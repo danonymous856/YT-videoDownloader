4e9e7b688f6f48abb0b5603edfc0625cd77db4c3
D Anonymous <81114860+danonymous856@users.noreply.github.com>
2021-10-30 12:15:47 +0530

    littl3 changes

--- dowload.py
+++ dowload.py
@@ -9,24 +9,24 @@ yt=YouTube(link)
 print(yt.streams.filter(type='audio'))
 audio=yt.streams.get_by_itag(input())  #Recommended 139
 print('Downloading Audio.....')
-APATH ="E:\Bootcamp\Aud/"    #to_do for audio
+APATH ="C:\DSA Bootcamp\Aud/"    #to_do for audio
 audio_filename=audio.title
 print(audio_filename)
 audio.download(APATH)
-print("Audio Downloaded")
+print("Audio Downloaded \n\n\n")
 
 # for video
 print(yt.streams.filter(progressive=False,type='video'))
 print('Enter the itag:')
 video=yt.streams.get_by_itag(input())
 print('Downloading Video.....')
-VPATH = 'E:\Bootcamp\Vid/'     #to_do for video
+VPATH = 'C:\DSA Bootcamp\Vid/'     #to_do for video
 video_filename=video.title
 print(video_filename)
+print(video.filesize)
 video.download(VPATH)
 print("Video Downloaded")
 
-
 # Merging code giving some error due to which we have to work with ffmpeg on the terminal
-# just to right click ffmpeg(.exe) inside "C:\Users\____\Desktop\YT-VID\ffmpeg\bin\"
+# just to right click ffmpeg(.exe) inside "C:\Users\Donald\Desktop\YT-VID\ffmpeg\bin\"
 # and type "ffmpeg -i video.mp4 -i audio.mp4 -c:v copy -c:a aac output.mp4"
