b3f1bc7f8c2e2ced7266a2f0746605c7f873ff96
D Anonymous <81114860+danonymous856@users.noreply.github.com>
2021-11-03 12:36:55 +0530

    just commit

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ yt=YouTube(link)
 print(yt.streams.filter(type='audio'))
 audio=yt.streams.get_by_itag(input())
 print('Downloading Audio......')
-APath = "C:\DSA Bootcamp\Aud/"
+APath = "F:\DSA Bootcamp\Aud/"
 
 audio_filename=audio.title
 print(audio_filename)
@@ -20,11 +20,10 @@ print(yt.streams.filter(progressive=False,type='video'))
 print('Enter the itag:')
 video=yt.streams.get_by_itag(input())
 print('Downloading Video......')
-Vpath = "C:\DSA Bootcamp\Vid/"
+Vpath = "F:\DSA Bootcamp\Vid/"
 video_filename=video.title
 print(video_filename)
 print(video.filesize)
-print(video.on_progress())
 video.download(Vpath)
 print("Video Downloaded")
 
@@ -37,4 +36,4 @@ print("Video Downloaded")
 
 # Merging code giving some error due to which we have to work with ffmpeg on the terminal
 # just to right click ffmpeg(.exe) inside "C:\Users\Donald\Desktop\YT-VID\ffmpeg\bin\"
-# and type "ffmpeg -i video.mp4 -i audio.mp4 -c:v copy -c:a aac output.mp4"7
+# and type "ffmpeg -i video.mp4 -i audio.mp4 -c:v copy -c:a aac output.mp4"
